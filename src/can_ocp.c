6cd22a18e3143d3344fd4205a9991dbd916f7ac4
Artem Smirnov <altemka@icloud.com>
2016-04-15 19:01:48 +0300

    Accomplished callback can23_ocp_remove

--- src/can_ocp.c
+++ src/can_ocp.c
@@ -11,14 +11,13 @@
 #include <linux/module.h>
 #include <linux/version.h>
 #include <linux/can/dev.h>
+#include <linux/clk.h>
 #include <linux/platform_device.h>
 
 #include "can_ocp.h"
 
-
-
 /*-----------------------------------------------------------------------------
------------------------ Definitions platform driver callbacks ------------------ 
+--------------------- Definitions platform driver callbacks ------------------- 
 -----------------------------------------------------------------------------*/
 
 static int can23_ocp_probe( struct platform_device *pdev )
@@ -26,9 +25,29 @@ static int can23_ocp_probe( struct platform_device *pdev )
 
 }
 
+/*----------------------- Remove module from kernel -------------------------*/
+
 static int can23_ocp_remove( struct platform_device *pdev )
 {
+	struct resource *res;
+	struct net_device *ndev = platform_get_drvdata( pdev );
+	struct can23_ocp *priv = netdev_priv( ndev );
 
+	// shut down and remove module from the kernel tables
+	unregister_netdev( ndev );
+
+	// free memory
+	iounmap( priv->reg_base );
+	res = platform_get_resource( pdev, IORESOURCE_MEM, 0 );
+	release_mem_region( res->start, resource_size( res ) );
+
+	// free clock source
+	clk_put( priv->clk );	// !!!!!!!!
+
+	// free can-device
+	free_candev( ndev );
+
+	return 0;
 }
  
 /*-----------------------------------------------------------------------------
